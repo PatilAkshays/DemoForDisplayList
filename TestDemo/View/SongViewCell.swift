9a4fb1c6d9f8b688872721b71597079ffb189427
Akshay <akshay.patil@treel.in>
2021-07-26 22:33:11 +0530

    first commit

---
+++ TestDemo/View/SongViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SongViewCell.swift
+//  TestDemo
+//
+//  Created by Manish Ingale on 26/07/21.
+//
+
+import UIKit
+
+class SongViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
